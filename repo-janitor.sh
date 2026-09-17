744d4f8cd2b9b409e6b7561b55412eec9b65bce2
Jean Decian <jeandecian@hotmail.fr>
2026-09-17 16:05:20 -0400

    feat(init): add initial script for repo janitor CLI workspace cleanup

---
+++ repo-janitor.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "=================================================="
+echo "                 REPO JANITOR CLI                 "
+echo "              Local Workspace Cleanup             "
+echo "=================================================="
